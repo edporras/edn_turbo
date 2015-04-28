289fd39e3162942d2a0fbf2c27536f1ce91146c9
Ed Porras <ed@motologic.com>
2015-04-28 11:42:32 +0200

    Tests to run w/ rake test

--- lib/edn_ext/edn_parser.rb
+++ lib/edn_ext/edn_parser.rb
@@ -5,9 +5,9 @@ module EDN_EXT
   # ----------------------------------------------------------------------------
   # instantiate a parser (defined in the C-side) and process the file
   #
-  def self.read(file)
+  def self.read(file, options = {})
 
-    $stderr.puts "EDN_ext #{VERSION}"
+    $stderr.puts "EDN_ext #{VERSION}" if options[:silent]
     Parser.new.parse(file)
 
   end
