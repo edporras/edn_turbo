7ad9cbb88b93b96c7bbb683c4cfcc564a368c352
Ed Porras <ed@motologic.com>
2015-07-07 14:38:42 -0400

    switch to non-templatized ruby wrapper functions because g++ on ubuntu 11/12 doesn't like them

--- ext/edn_turbo/main.cc
+++ ext/edn_turbo/main.cc
@@ -29,18 +29,16 @@ namespace edn {
 
     //
     // wrappers to hook the class w/ the C-api
-    template<class T>
-    static void delete_obj(T *ptr) {
+    static void delete_parser(edn::Parser *ptr) {
         delete ptr;
     }
 
-    template<class T>
-    static VALUE wrap_ptr(VALUE klass, T* ptr) {
-        return Data_Wrap_Struct(klass, 0, delete_obj<T>, ptr);
+    static VALUE wrap_parser_ptr(VALUE klass, edn::Parser* ptr) {
+        return Data_Wrap_Struct(klass, 0, delete_parser, ptr);
     }
 
     static VALUE alloc_obj(VALUE self){
-        return wrap_ptr<edn::Parser>(self, new Parser());
+        return wrap_parser_ptr(self, new Parser());
     }
 
     static inline Parser* get_parser(VALUE self)
