7dbb988620a7a5bcd24013c8ab893ba02cd94a45
Russ Olsen <russ@russolsen.com>
2015-07-21 11:01:24 -0400

    Crude rewiring to make edn_turbo a edn plugin.

--- lib/edn_turbo.rb
+++ lib/edn_turbo.rb
@@ -1,44 +1,19 @@
 require 'edn'
-#require_relative 'edn_turbo/constants'
-#require_relative 'edn_turbo/tags'
-require_relative 'edn_turbo/utils'
-require_relative 'edn_turbo/version'
-require_relative 'edn_turbo/edn_parser'
-require_relative 'edn_turbo/edn_turbo'
-
-require 'set'
+require 'edn_turbo/version'
+require 'edn_turbo/edn_parser'
+require 'edn_turbo/edn_turbo'
 
 module EDNT
-
-  # ----------------------------------------------------------------------------
   # instantiate a parser (defined in the C-side) and parse the file
-  #
   def self.read(input)
-
     begin
       data = input.instance_of?(String) ? input : input.read
 
       Parser.new.parse(data)
     rescue EOFError
     end
-
   end
+end
 
-  # ----------------------------------------------------------------------------
-  # handles creation of various EDN representations
-  #
-=begin
-  def self.symbol(elem)
-    EDN::Type::Symbol.new(elem)
-  end
-
-  def self.list(*elems)
-    EDN::Type::List.new(*elems)
-  end
-
-  def self.set(*elems)
-    Set.new(*elems)
-  end
-=end
-
-end # EDN namespace
+puts "rewirting parser"
+EDN::parser=EDNT::Parser
--- lib/edn_turbo/edn_parser.rb
+++ lib/edn_turbo/edn_parser.rb
@@ -1,5 +1,22 @@
-require 'edn'
-require 'bigdecimal'
-
 module EDNT
+  # Bind the given meta to the value.
+  def self.extend_for_meta(value, ext_meta)
+
+    meta = ext_meta
+
+    metadata = meta.reduce({}) do |acc, m|
+      case m
+      when Symbol then acc.merge(m => true)
+      when EDN::Type::Symbol then acc.merge(:tag => m)
+      else acc.merge(m)
+      end
+    end
+
+    if !metadata.empty?
+      value.extend EDN::Metadata
+      value.metadata = metadata
+    end
+
+    value
+  end
 end
