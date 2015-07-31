cffa86685ba767aadb2480fc4a04b724a45b9629
Russ Olsen <russ@russolsen.com>
2015-07-31 16:19:17 -0400

    Replace the EDN parser with the EDNT parser at load time

--- lib/edn_turbo.rb
+++ lib/edn_turbo.rb
@@ -3,6 +3,13 @@ require 'edn_turbo/version'
 require 'edn_turbo/edn_parser'
 require 'edn_turbo/edn_turbo'
 
+# Replace the parser in the EDN module with the C based one.
+module EDN
+  self.parser=EDNT::Parser
+end
+
+# Do we need this?
+#
 module EDNT
   # instantiate a parser (defined in the C-side) and parse the file
   def self.read(input)
