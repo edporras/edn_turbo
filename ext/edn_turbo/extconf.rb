20d7f160d255ab3b6a812d6619abd6b2f0c1555d
Ed Porras <ed@motologic.com>
2019-01-30 13:03:53 +0100

    Custom handling for macOS using pkg-config files bundled with icu4c to set search paths.

--- ext/edn_turbo/extconf.rb
+++ ext/edn_turbo/extconf.rb
@@ -2,16 +2,35 @@
 
 require 'mkmf'
 
-header_dirs = [
-  '/usr/local/include',
-  '/usr/local/opt/icu4c/include',
-  '/usr/include'
-].freeze
+header_dirs =
+  if RUBY_PLATFORM =~ /darwin/
+    abort "\n>> failed to find icu4c package - Did you run 'brew install icu4c'?\n\n" unless
+      File.exist?('/usr/local/opt/icu4c/lib/pkgconfig/icu-uc.pc')
 
-lib_dirs = [
-  '/usr/local/lib', # must be the first entry; add others after it
-  '/usr/local/opt/icu4c/lib'
-].freeze
+    i_opt = %x[ export PKG_CONFIG_PATH="/usr/local/opt/icu4c/lib/pkgconfig" && pkg-config --cflags-only-I icu-uc ]
+    [
+      i_opt[/-I(.+?)\s/,1]
+    ].freeze
+  else
+    [
+      '/usr/local/include',
+      '/usr/local/opt/icu4c/include',
+      '/usr/include'
+    ].freeze
+  end
+
+lib_dirs =
+  if RUBY_PLATFORM =~ /darwin/
+    l_opt = %x[ pkg-config --libs icu-uc ]
+    [
+      l_opt[/-L(.+?)\s/,1]
+    ].freeze
+  else
+    [
+      '/usr/local/lib', # must be the first entry; add others after it
+      '/usr/local/opt/icu4c/lib'
+    ].freeze
+  end
 
 dir_config('icuuc', header_dirs, lib_dirs)
 
